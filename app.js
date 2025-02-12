abc935a08deed0b3b5965a12e44de542ffa75fe9
irondroidmtz <jorge.amartinezmtz@gmail.com>
2025-02-11 21:55:12 -0600

    Se crea la funcion para actualizar lista de amigos

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = [];
+let nombreAmigo = "";
 
-function agregarAmigo(){
+function guardarAmigo(){
     const nombreAmigo = document.getElementById("amigo").value;
         if (nombreAmigo == ""){
             alert("Por favor, inserta un nombre valido");
         }
         else {
             listaAmigos.push(nombreAmigo);
-            document.getElementById("nombreAmigo").value = "";
+            document.getElementById("amigo").value = "";
             actualizarLista();
         }
 
 }
+
+function listaDeAmigos(){
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach(item => {
+        const li = document.createElement("li");
+        li.textContent = item;
+        lista.appendChild(li);
+    })
+
+    }
